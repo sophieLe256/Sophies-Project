f8b92941d3169cafff7770fefe825a26eeb5e4f9
sophieLe256 <102685323+sophieLe256@users.noreply.github.com>
2022-07-13 19:37:03 -0400

    Sophies starting project

---
+++ src/test/java/com/sophie/SophiesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SophiesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
